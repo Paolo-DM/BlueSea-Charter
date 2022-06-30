053c1d7d8e9d07ab16b00cc433a07cf0388a2d3c
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-06-30 15:57:40 +0000

    Fixed diving and sunset cruise href.

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -6,67 +6,74 @@ export default class Navbar extends React.Component {
   render() {
     const { user, handleSignOut } = this.context;
     return (
-    <nav className="navbar navbar-expand-md navbar-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand d-flex align-items-center" href="#">
-          <img
-            src="../img/logo.png"
-            className="logo-img"
-          ></img>
-        </a>
-          <a className="text-decoration-none text-white mx-2" aria-current="page" href="#">
-              BlueSea Charter
-            </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarNavAltMarkup"
-        >
-          <div className="navbar-nav">
+      <nav className="navbar navbar-expand-md navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand d-flex align-items-center" href="#">
+            <img src="../img/logo.png" className="logo-img"></img>
+          </a>
+          <a
+            className="text-decoration-none text-white mx-2"
+            aria-current="page"
+            href="#"
+          >
+            BlueSea Charter
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav">
+              <a className="nav-link" href="#excursions">
+                Excursions
+              </a>
+              <a className="nav-link" href="#fleet">
+                The Fleet
+              </a>
+              <a className="nav-link" href="#wedding">
+                Wedding Proposal
+              </a>
 
-            <a className="nav-link" href="#excursions">
-              Excursions
-            </a>
-            <a className="nav-link" href="#fleet">
-              The Fleet
-            </a>
-            <a className="nav-link" href="#wedding">
-              Wedding Proposal
-            </a>
-            <a className="nav-link" href="#">
-              Diving
-            </a>
-            <a className="nav-link" href="#">
-              Account
-            </a>
+              <a className="nav-link" href="#sunset">
+                Sunset Cruise
+              </a>
+              <a className="nav-link" href="#diving">
+                Diving
+              </a>
 
-             { user !== null &&
-               <button className="btn btn-sm btn-outline-light sign-btn" onClick={handleSignOut}>
-                 Sign out
-               </button>
-             }
-             { user === null &&
-               <>
-                 <a href="#sign-in" className="btn btn-sm btn-outline-light sign-btn mx-1">
-                   Sign In
-                 </a>
-               </>
-             }
-
+              {user !== null && (
+                <button
+                  className="btn btn-sm btn-outline-light sign-btn"
+                  onClick={handleSignOut}
+                >
+                  Sign out
+                </button>
+              )}
+              {user === null && (
+                <>
+                  <a
+                    href="#sign-in"
+                    className="btn btn-sm btn-outline-light sign-btn mx-1"
+                  >
+                    Sign In
+                  </a>
+                </>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>);
+      </nav>
+    );
   }
 }
 
